f21facd56050eaa0c47ad98a71079977b06be95e
SilvrHorison <jonathan.godar@gmail.com>
2020-11-02 19:39:59 +0100

    Created models

---
+++ app/models.py
@@ -0,0 +1,69 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
+from uuid import uuid4
+
+
+def generateUuid():
+    return uuid4().hex
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(32), unique=True, default=generateUuid, index=True)
+
+
+    email = db.Column(db.String(248), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    
+    is_admin = db.Column(db.Boolean)
+    
+    training_images = db.relationship("TrainingImage", backref="user", lazy='dynamic')
+
+    def __init__(self, email="only", password_plain="for test purpose", is_admin=False):
+        super()
+
+        self.email = email
+        self.public_id = generateUuid()
+
+        self.set_password(password_plain)
+
+    def set_password(self, password_plain):
+        self.password_hash = generate_password_hash(password_plain)
+
+    def check_password(self, to_check):
+        return check_password_hash(self.password_hash, to_check)
+
+
+class TrainingImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(32), unique=True, default=generateUuid, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(f'{User.__tablename__}.id'))
+    
+    image_url = db.Column(db.String(512), nullable=False, index=True)
+
+    classified_areas = db.relationship("ClassifiedArea", backref="image", lazy='dynamic')
+
+
+    def accessible_by(self, user):
+        return self.user_id == user.id or user.is_admin
+
+
+class ClassifiedArea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(32), unique=True, default=generateUuid, index=True)
+
+    classified_as = db.Column(db.String(256), index=True)
+    image_id = db.Column(db.Integer, db.ForeignKey(f'{TrainingImage.__tablename__}.id'))
+
+    x_position = db.Column(db.Integer)
+    y_position = db.Column(db.Integer)
+
+    width = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+
+    def accessible_by(self, user):
+        return image.accessible_by(user)
+
+
+
+    
